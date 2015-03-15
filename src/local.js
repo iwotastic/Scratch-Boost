dee9cc21cae071d6c9c59b30957f96d4616ea51b
Iwotastic <ianmorrill@icloud.com>
2015-03-15 15:54:01 -0400

    Create local.js

---
+++ src/local.js
@@ -0,0 +1,82 @@
+(function(ext) {
+    ext._shutdown = function() {};
+    ext._getStatus = function() {
+        return {
+            status: 2,
+            msg: 'Ready'
+        };
+    };
+    var path = location.pathname;
+    var pid = path.slice(10, path.length - 1);
+    ext.makeLocalVar = function(name) {
+        if (typeof(Storage) != 'undefined') {
+            if (localStorage.getItem('BLV' + name + pid)) {
+                console.log('Variable ' + name + ' already exists. :O')
+            } else {
+                localStorage.setItem('BLV' + name + pid, '0');
+                console.log('Local variables supported, variable ' + name + ' created. :)')
+            }
+        } else {
+            console.log('Local variables not supported. :/');
+        }
+    }
+    ext.getLocalVar = function(name) {
+        if (typeof(Storage) != 'undefined') {
+            if (localStorage.getItem('BLV' + name + pid)) {
+                console.log('Local variables supported, variable ' + name + ' has a value of ' + localStorage.getItem('BLV' + name + pid));
+                return localStorage.getItem('BLV' + name + pid);
+            } else {
+                console.log('Variable ' + name + ' does not exist. ;(')
+            }
+        } else {
+            console.log('Local variables not supported. :/');
+        }
+    }
+    ext.localVarExists = function(name) {
+        if (typeof(Storage) != 'undefined') {
+            if (localStorage.getItem('BLV' + name + pid)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            console.log('Local variables not supported. :/');
+            return false;
+        }
+    }
+    ext.delLocalVar = function(name) {
+        if (typeof(Storage) != 'undefined') {
+            if (localStorage.getItem('BLV' + name + pid)) {
+                localStorage.removeItem('BLV' + name + pid);
+                console.log('Local variables supported, variable ' + name + ' deleted. :)');
+            } else {
+                console.log('Variable ' + name + ' does not exist. ;(')
+            }
+        } else {
+            console.log('Local variables not supported. :/');
+        }
+    }
+
+    ext.setLocalVar = function(name, val) {
+        if (typeof(Storage) != 'undefined') {
+            if (localStorage.getItem('BLV' + name + pid)) {
+                localStorage.setItem('BLV' + name + pid, val);
+                console.log('Local variables supported, variable ' + name + ' has a value of ' + localStorage.getItem('BLV' + name + pid));
+            } else {
+                console.log('Variable ' + name + ' does not exist. ;(');
+            }
+        } else {
+            console.log('Local variables not supported. :/');
+        }
+    }
+    var descriptor = {
+        blocks: [
+            [' ', 'create local variable %s', 'makeLocalVar', 'x'],
+            ['r', 'local variable %s', 'getLocalVar', 'x'],
+            [' ', 'set local variable %s to %s', 'setLocalVar', 'x', 'hi'],
+            [' ', 'delete local variable %s', 'delLocalVar', 'x'],
+            ['b', 'local variable %s exists', 'localVarExists', 'x']
+        ]
+    };
+    ScratchExtensions.register('Boost - Local Variables', descriptor, ext);
+})({});
