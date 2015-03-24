9e7576c1ffc8003e41dd37633016ded5d0ecd7f1
ealgase <mostdigitsofpi@gmail.com>
2015-03-24 17:41:13 -0400

    Update jsmath.js

--- src/jsmath.js
+++ src/jsmath.js
@@ -1,17 +1,17 @@
 (function(ext) {
-	ext._shutdown = function() {};
+	ext._shutdown = function() {}
     ext._getStatus = function() {
-        return {status: 2, msg: 'Ready'};
+        return {status: 2, msg: 'Ready'}
     };
-    ext.pi = function(){return Math.PI;};
-    ext.e = function(){return Math.E;};
-    ext.e = function(num){return Math.Log(num);};
+    ext.pi = function(){return Math.PI}
+    ext.e = function(){return Math.E}
+    ext.e = function(num){return Math.Log(num)}
     var descriptor = {
         blocks: [
             ['r', 'π(Pi)', 'pi'],
             ['r', "E", 'e'],
             ['r', "log %n", 'log', '2'],
         ]
-    };
-    ScratchExtensions.register('Boost - JavaScript Math', descriptor, ext);
-})({});
+    }
+    ScratchExtensions.register('Boost - JavaScript Math', descriptor, ext)
+})({})
