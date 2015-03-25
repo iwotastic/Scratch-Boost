9830daed74957979f200d79f5e143bd9c213a121
ealgase <mostdigitsofpi@gmail.com>
2015-03-24 20:44:58 -0400

    Update color.js
    Bug fix

--- src/color.js
+++ src/color.js
@@ -8,7 +8,7 @@
         ext.rgb = function(r, g, b) {
         return ((r*65536)+(g*256)+b);
     }
-        ext.randcolor - function(){return Math.round(Math.random()*100000000)}
+        ext.randcolor = function(){return Math.round(Math.random()*100000000)}
     var descriptor = {
         blocks: [
             ['r', 'color r: %n g: %n b: %n', 'rgb', 255, 255, 255],
