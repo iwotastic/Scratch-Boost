669e3ba3247dc295f820a11c590eb648c4afc588
ealgase <mostdigitsofpi@gmail.com>
2015-03-24 20:31:51 -0400

    Update jsmath.js

--- src/jsmath.js
+++ src/jsmath.js
@@ -5,13 +5,11 @@
     };
     ext.pi = function(){return Math.PI}
     ext.e = function(){return Math.E}
-    ext.sqrt12 = function(){return Math.SQR1_2}
     ext.exp = function(num1,num2){return Math.pow(num1,num2)}
     var descriptor = {
         blocks: [
             ['r', 'π(Pi)', 'pi'],
             ['r', "E", 'e'],
-            ['r', "Square root of 1/2", 'sqrt12'],
             ['r', "%n ^ %n", 'exp','2','4'],
         ]
     }
