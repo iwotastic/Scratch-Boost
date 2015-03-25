b6ce01b92b62685a29abe3abc80d57af816f8d51
ealgase <mostdigitsofpi@gmail.com>
2015-03-24 20:30:43 -0400

    Update jsmath.js

--- src/jsmath.js
+++ src/jsmath.js
@@ -5,12 +5,14 @@
     };
     ext.pi = function(){return Math.PI}
     ext.e = function(){return Math.E}
-    ext.log = function(num){return Math.log(num)}
+    ext.sqrt12 = function(){return Math.SQR1_2}
+    ext.exp = function(num1,num2){return Math.pow(num1,num2)}
     var descriptor = {
         blocks: [
             ['r', 'π(Pi)', 'pi'],
             ['r', "E", 'e'],
-            ['r', "log %n", 'log', '2'],
+            ['r', "Square root of 1/2", 'sqrt12'],
+            ['r', "%n ^ %n", 'exp','2','4'],
         ]
     }
     ScratchExtensions.register('Boost - JavaScript Math', descriptor, ext)
