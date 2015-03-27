73830581803046e1dff84ef64aa03e305dbad436
ealgase <mostdigitsofpi@gmail.com>
2015-03-27 16:22:26 -0400

    Update jsmath.js

--- src/jsmath.js
+++ src/jsmath.js
@@ -5,21 +5,21 @@
     };
     ext.pi = function(){return Math.PI}
     ext.e = function(){return Math.E}
-    //ext.math = function(num1,operation,num2){
-        //if operation=='+'{return num1+num2}
-        //if operation=='-'{return num1-num2}
-        //if operation=='*'{return num1*num2}
-        //if operation=='/'{return num1/num2}
-        //if operation=='^'{return Math.pow(num1,num2)}
-    //};
+    ext.math = function(num1,operation,num2){
+        if (operation=='+'){return Number(num1)+Number(num2)}
+        if (operation=='-'){return num1-num2}
+        if (operation=='*'){return num1*num2}
+        if (operation=='/'){return num1/num2}
+        if (operation=='^'){return Math.pow(num1,num2)}
+    };
     var descriptor = {
         blocks: [
             ['r', 'π(Pi)', 'pi'],
             ['r', "E", 'e'],
-            //['r', "%n %m.math %n", 'math','2','+','2'],
+            ['r', "%n %m.math %n", 'math','2','+','2'],
         ],
         menus:{
-        //math: ['+','-','*','/','^'],
+        math: ['+','-','*','/','^'],
         }
     };
     ScratchExtensions.register('Boost - JavaScript Math', descriptor, ext);
