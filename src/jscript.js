d15adbf10f3cc82502a10c28329ab21461153066
ealgase <mostdigitsofpi@gmail.com>
2015-03-29 07:35:16 -0400

    Update jscript.js

--- src/jscript.js
+++ src/jscript.js
@@ -27,49 +27,49 @@
             }
         }
     };
-    ext.fetch=function(url,data,sucsess){
-jQuery.get( url , data  , success){
-    }
-    ext.jsvar=function(var){
-    	switch(var)
-    	    case var1:
-    	    return var1
-    	    break;
-    	    case var2:
-    	    return var2
-    	    break;
-    	    case var3:
-    	    return var3
-    	    break;
-    	    case var4:
-    	    return var4
-    	    break;
-    	    case var5:
-    	    return var5
-    	    break;
-    	    case var6:
-    	    return var6
-    	    break;
-    	    case var7:
-    	    return var7
-    	    break;
-    	    case var8:
-    	    return var8
-    	    break;
-    	    case var9:
-    	    return var9
-    	    break;
-    	    case var10:
-    	    return var10
-    	    break;
-    	
-    }
+//    ext.fetch=function(url,data,sucsess){
+//jQuery.get( url , data  , success){
+ //   }
+//    ext.jsvar=function(var){
+ //   	switch(var)
+  //  	    case var1:
+   // 	    return var1
+   // 	    break;
+    //	    case var2:
+   // 	    return var2
+   // 	    break;
+   // 	    case var3:
+   // 	    return var3
+   // 	    break;
+   // 	    case var4:
+   // 	    return var4
+   // 	    break;
+    //	    case var5:
+   // 	    return var5
+   // 	    break;
+   // 	    case var6:
+   // 	    return var6
+   // 	    break;
+   // 	    case var7:
+   // 	    return var7
+   // 	    break;
+   // 	    case var8:
+   // 	    return var8
+   // 	    break;
+    //	    case var9:
+   // 	    return var9
+   // 	    break;
+   // 	    case var10:
+   // 	    return var10
+   // 	    break;
+   // 	
+    //}
     var descriptor = {
         blocks: [
             [' ', 'import Scratch extension from URL: %s', 'imp'],
             [' ', 'run JS %s', 'exe', 'alert("Scratch is awesome!");'],
-            [' ', 'fetch website %s with data %s sucsess function %s', 'fetch', '', '', ''],
-            [' ', 'JS var %m.jsvar', 'jsvar', 'var1'],
+    //        [' ', 'fetch website %s with data %s sucsess function %s', 'fetch', '', '', ''],
+    //        [' ', 'JS var %m.jsvar', 'jsvar', 'var1'],
         ],
         menus:{
         	jsvar:['var1','var2','var3','var4','var5','var6','var7','var8','var9','var10']
