3ed2337433cbba950ea2a4ed44e60d20a153754a
Iwotastic <ianmorrill@icloud.com>
2015-06-05 16:31:11 -0400

    Update installer.js

--- src/installer.js
+++ src/installer.js
@@ -6,13 +6,6 @@ ScratchExtensions.loadExternalJS('https://rawgit.com/Iwotastic/Scratch-Boost/mas
 ScratchExtensions.loadExternalJS('https://rawgit.com/Iwotastic/Scratch-Boost/master/src/jscript.js');
 ScratchExtensions.loadExternalJS('https://rawgit.com/Iwotastic/Scratch-Boost/master/src/browser.js');
 $(".box-head > .buttons").append('<div class="button" onclick="info()"><span class="white">Scratch Boost</span></div>');
-var inf = "";
 function info() {
-    if (inf === "") {
-        inf = $("#info").html();
-        $("#info").html('<div class="dynamic"><div class="text-block"><b>Scratch Boost Info</b><div class="read-only instructions" style="height: 161.5px;"><p>Scratch Boost by: Iwotastic<br />View on <a href="https://github.com/Iwotastic/Scratch-Boost">GitHub</a><br />View <a href="https://github.com/Iwotastic/Scratch-Boost">Scratch discussion topic</a></p></div></div></div>');    
-      }else{
-        $("#info").html(inf);
-        inf = "";
-    }
+    window.open("http://iwotastic.github.io/Scratch-Boost/connect/");
 }
