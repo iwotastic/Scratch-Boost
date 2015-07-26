0b58948ef81320e442c7966abbd7a63fb5fc9b21
Iwotastic <ianmorrill@icloud.com>
2015-07-25 21:04:40 -0400

    Update objects.js

--- src/objects.js
+++ src/objects.js
@@ -4,9 +4,21 @@
     ext._getStatus = function() {
         return {status: 2, msg: 'Ready'};
     };
+    ext.nObj = function() {
+        return '{}';
+    };
+    ext.rObjpoObj = function(pro, obj) {
+        return JSON.stringify(JSON.parse(obj)[pro]);
+    };
+    ext.wpoObj = function(pro, obj, val) {
+        return JSON.stringify(JSON.parse(obj)[pro] = val);
+    };
     var descriptor = {
         blocks: [
-            [' ', 'alert %s', 'say', 'Hello!'],
+            ['r', 'create object', 'nObj'],
+            ['r', 'object %s of %s', 'rObjpoObj', 'Friends', null],
+            ['r', '%s of %s', 'rpoObj', 'Name', null],
+            ['r', 'set %s of %s to %s', 'wpoObj', 'Name', null, 'Billy'],
         ]
     }; 
     ScratchExtensions.register('Boost - Objects', descriptor, ext);
