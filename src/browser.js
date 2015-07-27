9c5a2a2fe7ce2e816056c6c56e8eb6ba73c6c14e
Iwotastic <ianmorrill@icloud.com>
2015-07-27 11:21:57 -0400

    Update browser.js

--- src/browser.js
+++ src/browser.js
@@ -33,7 +33,8 @@
             ['r', 'prompt %s with default value %s', 'defv', 'What is your favorite color?', 'Purple'],
             [' ', 'set tab name to %s', 'setTab', 'Hey!'],
             ['r', 'tab name', 'tabN'],
-        ]
+        ],
+        url: "http://Iwotastic.github.io/Scratch-Boost/index.html#browser"
     }; 
     ScratchExtensions.register('Boost - Browser', descriptor, ext);
 })({});
