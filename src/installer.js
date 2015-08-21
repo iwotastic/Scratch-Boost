5b6dece382dd753782592b11560fcc6d41d151e4
Ian Morrill <ianmorrill@icloud.com>
2015-08-21 17:29:34 -0400

    update

--- src/installer.js
+++ src/installer.js
@@ -8,9 +8,9 @@ ScratchExtensions.loadExternalJS('https://rawgit.com/Iwotastic/Scratch-Boost/mas
 ScratchExtensions.loadExternalJS('https://rawgit.com/Iwotastic/Scratch-Boost/master/src/objects.js');
 $.get('https://rawgit.com/Iwotastic/Scratch-Boost/master/src/dialog_template', function(data){
 	$("body").append(data);
-	$(".sb-template").children().hide();
+	$(".sb-template").hide();
 });
 $(".box-head > .buttons").append('<div class="button" id="see-inside" onclick="info()"><span class="white">Scratch Boost</span></div>');
 function info() {
-	$("#aboutWin").show().css({"position": "fixed", "top": "40%", "left": "40%"});
+	window.open("http://iwotastic.github.io/Scratch-Boost", "", "width=400, height=500"); 
 }
