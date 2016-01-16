d9c6126f8ddd0bbd082faee5b2c228c4a7d7b899
Ian Morrill <ianmorrill@icloud.com>
2016-01-16 11:01:27 -0500

    ⭐️ Add SweetAlert ⭐️

--- src/installer.js
+++ src/installer.js
@@ -1,3 +1,9 @@
+var sast = document.createElement("link");
+sast.setAttribute("src", "https://rawgit.com/Iwotastic/Scratch-Boost/master/src/sweetalert.css");
+document.head.appendChild(sast);
+var sasc = document.createElement("script");
+sasc.setAttribute("src", "https://rawgit.com/Iwotastic/Scratch-Boost/master/src/sweetalert.min.js");
+document.body.appendChild(sasc);
 /* fixed by MrSherlockHolmes */
 ScratchExtensions.loadExternalJS("https://rawgit.com/Iwotastic/Scratch-Boost/master/src/strings.js");
 ScratchExtensions.loadExternalJS("https://rawgit.com/Iwotastic/Scratch-Boost/master/src/scratch.js");
@@ -7,6 +13,7 @@ ScratchExtensions.loadExternalJS("https://rawgit.com/Iwotastic/Scratch-Boost/mas
 ScratchExtensions.loadExternalJS("https://rawgit.com/Iwotastic/Scratch-Boost/master/src/browser.js");
 ScratchExtensions.loadExternalJS("https://rawgit.com/Iwotastic/Scratch-Boost/master/src/objects.js");
 document.querySelector(".box-head > .buttons").innerHTML = document.querySelector(".box-head > .buttons").innerHTML + "<div class=\"button\" onclick=\"info()\"><span class=\"white\">Scratch Boost</span></div>";
+swal("Good to Go!", "Scratch Boost is installed", "success");
 function info() {
 	window.open("http://iwotastic.github.io/Scratch-Boost", "", "width=400, height=500");
 }
